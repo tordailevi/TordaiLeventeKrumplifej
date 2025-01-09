fe866c58dbab5ab384daa6b82ae88873b03c8492
Torderius <tordailevi@gmail.com>
2025-01-09 09:18:10 +0100

    keesz

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import regisztracio
 import sorozat
 import megrendelok
 
-'''regisztracio.feladat1()
+regisztracio.feladat1()
 randomszamok = sorozat.feladat2()
 szamlalo = sorozat.nagyobb(randomszamok)
 sorozat.konzol_ir(szamlalo)
-sorozat.fajba_ir(szamlalo)'''
+sorozat.fajba_ir(szamlalo)
 megrendeles_lista = megrendelok.filebeolvasas()
 megrendelok.rendelesszam(megrendeles_lista)
 megrendelok.krumplifej(megrendeles_lista)
